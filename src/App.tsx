c48bfb902c7d047f02feee1723a97004e6bf5f52
dozenkittens <d0bryak274@yandex.ru>
2026-06-19 17:36:44 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+// import { HorseIcon, HeartIcon, CubeIcon } from "@phosphor-icons/react";
+
+
+function App() {
+
+
+  return (
+    <MantineProvider>
+      <
+    </MantineProvider>
+  )
+}
+
+export default App
